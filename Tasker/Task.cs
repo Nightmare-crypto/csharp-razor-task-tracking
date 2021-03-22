0ddd35fd9dfa8b428be6ff17081e5975aa63b0b4
Fabian Franz <fabian.franz@yahoo.de>
2021-03-22 20:03:41 +0100

    made properties required via data-annotations

--- Tasker/Task.cs
+++ Tasker/Task.cs
@@ -1,10 +1,18 @@
-﻿namespace Tasker
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tasker
 {
     public class Task
     {
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public int Priority { get; set; }
     }
 }
